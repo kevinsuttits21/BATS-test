8798627b865bb1e1eedf5d8e41b4a53b2b83067e
darealkevin <93242927+kevinsuttits21@users.noreply.github.com>
2023-11-09 13:42:37 +0200

    uuendus script.sh failile

--- BATS/script.sh
+++ BATS/script.sh
@@ -1,17 +1,35 @@
 #!/usr/bin/env bash
 
-#make a call
-response=$(curl -X GET https://jsonplaceholder.typicode.com/todos/1)
-echo $response
+function getTask(){
+    local taskId=$1
+    #make a call
+    response=$(curl -X GET https://jsonplaceholder.typicode.com/todos/"$taskId")
+    echo $response
 
-#get status of task wihtout jq or helpers
-echo $response | awk -F: '{print $5}' | awk '{print $1}'
-status=$(echo $response | awk -F: '{print $5}' | awk '{print $1}')
+    #get status of task wihtout jq or helpers
+    echo $response | awk -F: '{print $5}' | awk '{print $1}'
+    status=$(echo $response | awk -F: '{print $5}' | awk '{print $1}')
+}
 
-if [ "$status" != "false" ]; then
-  echo "Status of task completion is not correct: $status"
-  exit 1
-else
-  echo "Status of task completion is correct: $status"
-  echo "$status" > file.txt
+function saveResponse(){
+  local fileName=$1
+  local status=$2
+  if [ "$status" != "false" ]; then
+    echo "Status of task completion is not correct: $status"
+    exit 1
+  else
+    echo "Status of task completion is correct: $status"
+    echo "$status" > "$fileName"
+  fi
+}
+
+function run_main(){
+  local taskId=$1
+  local fileName=$2
+  getTask "$taskId"
+  saveResponse "$fileName" "$status"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  run_main $1 $2
 fi
